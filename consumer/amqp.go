a5515499f190e81b1314092f08c9a0b8ecb5f847
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-24 00:03:25 -0400

    endc

---
+++ consumer/amqp.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+var (
+	amqpURI = flag.String("amqp", "amqp://guest:guest@172.17.0.5:5672/", "AMQP URI")
+)
+
+var conn *amqp.Connection
+var ch *amqp.Channel
+var replies <-chan amqp.Delivery
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+func run() {
+	flag.Parse()
+	var err error
+	var q amqp.Queue
+
+	conn, err = amqp.Dial(*amqpURI)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	log.Printf("got Connection, getting Channel...")
+
+	ch, err = conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer conn.Close()
+
+	q, err = ch.QueueDeclare(
+		"order-queue", // name, leave empty to generate a unique name
+		true,          // durable
+		false,         // delete when usused
+		false,         // exclusive
+		false,         // noWait
+		nil,           // arguments
+	)
+	failOnError(err, "Error declaring the Queue")
+
+	log.Printf("declared Queue (%q %d messages, %d consumers), binding to Exchange (key %q)",
+		q.Name, q.Messages, q.Consumers, "go-test-key")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Error consuming the Queue")
+
+	forever := make(chan bool)
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
